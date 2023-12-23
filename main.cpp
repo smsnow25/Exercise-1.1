58659ce2c50a01d65e9e2ae10abb03290f3e4423
smsnow25 <139315216+smsnow25@users.noreply.github.com>
2023-12-23 17:57:15 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/* 1.1 Write a program that displays Welcome to C++, Welcome to Computer Science, and Programming is fun */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to C++" << endl;
+    cout << "Welcome to Computer Science" << endl;
+    cout << "Programming is fun" << endl;
+    
+    return 0;
+}
